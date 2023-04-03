6047213b06e82052c36a0be194ea42e6a83b7683
Ahmed Lafeer <lafeer92@gmail.com>
2023-04-03 13:21:34 +0800

    add discount codes and conditions

--- utils.js
+++ utils.js
@@ -1,3 +1,38 @@
+// define discount codes and their corresponding discount rates
+const DISCOUNT_CODES = {
+  OFR001: {
+    discountRate: 0.1,
+    discountApplicable: (pkg) => {
+      const { pkgDistance, pkgWeight } = pkg;
+      return pkgDistance < 200 && pkgWeight >= 70 && pkgWeight <= 200;
+    },
+  },
+  OFR002: {
+    discountRate: 0.07,
+    discountApplicable: (pkg) => {
+      const { pkgDistance, pkgWeight } = pkg;
+      return (
+        pkgDistance >= 50 &&
+        pkgDistance <= 150 &&
+        pkgWeight >= 100 &&
+        pkgWeight <= 250
+      );
+    },
+  },
+  OFR003: {
+    discountRate: 0.05,
+    discountApplicable: (pkg) => {
+      const { pkgDistance, pkgWeight } = pkg;
+      return (
+        pkgDistance >= 50 &&
+        pkgDistance <= 250 &&
+        pkgWeight >= 10 &&
+        pkgWeight <= 150
+      );
+    },
+  },
+};
+
 function createPackageObjects(pkgDetails, numOfPackages) {
   const packages = [];
   for (let i = 0; i < numOfPackages; i++) {
