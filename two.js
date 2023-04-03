05e78a70f0652b9c1a1dcb3be07911e2c4944f62
Ahmed Lafeer <lafeer92@gmail.com>
2023-04-03 13:26:29 +0800

    parse input arguments

---
+++ two.js
@@ -0,0 +1,11 @@
+const {
+  createPackageObjects,
+  calculatePackageCostsAndDiscounts,
+} = require("./utils.js");
+
+// parse input arguments
+const [baseDeliveryCost, numOfPackages] = process.argv.slice(2, 4).map(Number);
+const [...packageDetails] = process.argv.slice(4, -3);
+const [numOfVehicles, maxSpeed, maxCarriableWeight] = process.argv
+  .slice(-3)
+  .map(Number);
