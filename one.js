9accdbc28161a7396999f54a7edd117916d814e5
Ahmed Lafeer <lafeer92@gmail.com>
2023-04-03 13:22:55 +0800

    add function to calculate package costs and discounts

--- one.js
+++ one.js
@@ -1,4 +1,7 @@
-const { createPackageObjects } = require("./utils.js");
+const {
+  createPackageObjects,
+  calculatePackageCostsAndDiscounts,
+} = require("./utils.js");
 
 // parse input arguments
 const [baseDeliveryCost, numOfPackages] = process.argv.slice(2, 4).map(Number);
@@ -6,3 +9,15 @@ const [...packageDetails] = process.argv.slice(4);
 
 // parse package details into package objects
 const packages = createPackageObjects(packageDetails, numOfPackages);
+
+const packagesWithCostsAndDiscounts = calculatePackageCostsAndDiscounts(
+  packages,
+  baseDeliveryCost
+);
+
+// print delivery cost and discount for each package
+packagesWithCostsAndDiscounts.forEach((pkg) => {
+  console.log(pkg.pkgId, pkg.discount, pkg.pkgCost);
+});
+
+module.exports = { calculatePackageCostsAndDiscounts };
--- utils.js
+++ utils.js
@@ -33,6 +33,32 @@ const DISCOUNT_CODES = {
   },
 };
 
+// calculate package costs and discounts
+function calculatePackageCostsAndDiscounts(packages, baseDeliveryCost) {
+  for (const pkg of packages) {
+    const { pkgWeight, pkgDistance, offerCode } = pkg;
+    let pkgCost = baseDeliveryCost + pkgWeight * 10 + pkgDistance * 5;
+    let discount = 0;
+
+    // apply discount if applicable
+    if (offerCode && offerCode in DISCOUNT_CODES) {
+      const offer = DISCOUNT_CODES[offerCode];
+      const discountRate = offer.discountRate;
+      const discountApplicable = offer.discountApplicable(pkg);
+
+      if (discountApplicable) {
+        discount = Number((discountRate * pkgCost).toFixed(2));
+        pkgCost -= discount;
+      }
+    }
+
+    pkg.discount = discount;
+    pkg.pkgCost = pkgCost;
+  }
+
+  return packages;
+}
+
 function createPackageObjects(pkgDetails, numOfPackages) {
   const packages = [];
   for (let i = 0; i < numOfPackages; i++) {
@@ -52,4 +78,4 @@ function createPackageObjects(pkgDetails, numOfPackages) {
   return packages;
 }
 
-module.exports = { createPackageObjects };
+module.exports = { createPackageObjects, calculatePackageCostsAndDiscounts };
