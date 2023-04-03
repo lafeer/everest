0c5aa648deb7d2b8d7dc0b5a11f6eb2ceeb5d879
Ahmed Lafeer <lafeer92@gmail.com>
2023-04-03 13:32:54 +0800

    add test for sorting shipments

--- tests/two.test.js
+++ tests/two.test.js
@@ -1,4 +1,4 @@
-const { groupPackages } = require("../two.js");
+const { groupPackages, sortShipments } = require("../two.js");
 
 const groupedPackages = [
   [
@@ -104,5 +104,62 @@ describe("group packages into shipments and calculates package delivery times", 
     expect(Array.isArray(result)).toBe(true);
     expect(result.length).toBe(4);
     expect(result).toEqual(expectedResult);
+  });
+
+  test("sortShipments", () => {
+    const result = groupedPackages.sort(sortShipments);
+
+    expect(Array.isArray(result)).toBe(true);
+    expect(result.length).toBe(4);
+    expect(result).toEqual([
+      [
+        {
+          pkgId: "PKG4",
+          pkgWeight: 110,
+          pkgDistance: 60,
+          offerCode: "OFR002",
+          discount: 105,
+          pkgCost: 1395,
+        },
+        {
+          pkgId: "PKG2",
+          pkgWeight: 75,
+          pkgDistance: 125,
+          offerCode: "OFR008",
+          discount: 0,
+          pkgCost: 1475,
+        },
+      ],
+      [
+        {
+          pkgId: "PKG3",
+          pkgWeight: 175,
+          pkgDistance: 100,
+          offerCode: "OFR003",
+          discount: 0,
+          pkgCost: 2350,
+        },
+      ],
+      [
+        {
+          pkgId: "PKG5",
+          pkgWeight: 155,
+          pkgDistance: 95,
+          offerCode: "NA",
+          discount: 0,
+          pkgCost: 2125,
+        },
+      ],
+      [
+        {
+          pkgId: "PKG1",
+          pkgWeight: 50,
+          pkgDistance: 30,
+          offerCode: "OFR001",
+          discount: 0,
+          pkgCost: 750,
+        },
+      ],
+    ]);
   });
 });
